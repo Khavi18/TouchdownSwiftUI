3bba0e115f33c7df3c1e1e3d9072a9a2aa966d6d
Khavi18 <khavishini18@gmail.com>
2024-03-17 14:11:32 +0800

    Initial Commit

---
+++ TouchdownSwiftUI/TouchdownSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownSwiftUIApp.swift
+//  TouchdownSwiftUI
+//
+//  Created by Khavishini on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
